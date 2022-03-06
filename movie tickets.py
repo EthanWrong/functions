32d0b6247932843787dfeba15ef5ab2362d469f0
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-03-07 11:04:15 +1300

    create movie tickets.py

---
+++ movie tickets.py
@@ -0,0 +1,48 @@
+# NB: for some functions I have asked the user to input values, and for others I have called the values myself.
+#     I want to show that I can do both.
+
+# exercise 12
+def ticket_sales(notices):
+    print("Welcome to the Reading Cinemas Pro Maximum Ticket Sales software! ")
+    print(f"Today's notices include: {notices}")
+    print()
+    want_to_sell = "y"
+    a_sales = 0
+    s_sales = 0
+    c_sales = 0
+    g_sales = 0
+    while want_to_sell != "n":
+        want_to_sell = input("Do you want to sell a ticket (Y/N): ").lower()
+        if want_to_sell == "y":
+            number_of_tickets_to_buy = int(input("How many tickets do you want to buy?"))
+            ticket_to_buy = input(
+                "What type of ticket do you want? \nA for adult, or \nS for student, or \nC for child, or \nG for gift voucher, or \nAnything else to cancel \n>> ").lower()
+            if ticket_to_buy == "a":
+                a_sales += number_of_tickets_to_buy
+            elif ticket_to_buy == "s":
+                s_sales += number_of_tickets_to_buy
+            elif ticket_to_buy == "c":
+                c_sales += number_of_tickets_to_buy
+            elif ticket_to_buy == "g":
+                g_sales += number_of_tickets_to_buy
+            else:
+                print("Ticket sale cancelled. ")
+
+        elif want_to_sell == "n":
+            total_sales = a_sales*12.50 + s_sales*9.00 + c_sales*7.00
+            print("=============================================")
+            print(f"The total tickets sold today was {a_sales+s_sales+c_sales+g_sales}")
+            print("This was made up of: ")
+            print(f"{a_sales} for adults; and")
+            print(f"{s_sales} for students; and")
+            print(f"{c_sales} for children; and")
+            print(f"{g_sales}  gift vouchers")
+            print()
+            print(f"Sales for the day came to ${total_sales:.2f}")
+            print("=============================================")
+        print()
+
+
+# MAIN ROUTINE
+
+ticket_sales("Out of buttered popcorn till Wednesday.")
